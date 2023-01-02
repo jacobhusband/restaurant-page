f151c5a61ba39d921960fbef6043a76279901016
Jacob Husband <jakehusband2@gmail.com>
2023-01-01 20:07:37 -0800

    Completed menu items page

--- src/js/pages/menu.js
+++ src/js/pages/menu.js
@@ -8,13 +8,38 @@ export default class Menu extends Builder {
 
   pageCreation() {
     return this.buildElement("div", { class: "menu p-2r" }, [
-      this.buildElement(
-        "div",
-        {
-          class: "flex flex-dir-col align-items-center",
-        },
-        [this.buildElement("h2", { class: "mb-1r", textContent: "Items" })]
-      ),
+      this.buildElement("div", {}, [
+        this.buildElement("h2", { class: "mb-1r", textContent: "Items" }),
+        this.buildElement("div", { class: "menu-cards flex flex-wrap" }, [
+          this.createMenuItem(
+            "Balsamic Kabob",
+            "/src/images/kabobs/balsamic.jpg"
+          ),
+          this.createMenuItem(
+            "Grilled Kabob",
+            "/src/images/kabobs/grilled.jpg"
+          ),
+          this.createMenuItem(
+            "Mediterranean Kabob",
+            "/src/images/kabobs/mediterranean.jpg"
+          ),
+          this.createMenuItem(
+            "Seasoned Kabob",
+            "/src/images/kabobs/seasoned.jpg"
+          ),
+        ]),
+      ]),
+    ]);
+  }
+
+  createMenuItem(title, url) {
+    return this.buildElement("div", { class: "col-half card-container" }, [
+      this.buildElement("div", { class: "card" }, [
+        this.buildElement("h4", { textContent: title, class: "p-05r pb-0" }),
+        this.buildElement("div", { class: "img-container p-05r" }, [
+          this.buildElement("img", { src: url }),
+        ]),
+      ]),
     ]);
   }
 }
